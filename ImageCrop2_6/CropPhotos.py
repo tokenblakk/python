d9ec59fcd6f3ab140532f95415980fb3cb05fdd8
Tramel Jones <tokenblakk@gmail.com>
2015-04-30 19:38:47 -0500

    Junk removal. Removed code that does not belong to me. Libraries are intact.

--- ImageCrop2_6/CropPhotos.py
+++
@@ -1,4 +0,0 @@
-from Crop import *
-logging.info('Moving and Cropping. Origin: %s  Destination: %s', org,  dest)
-moveCrop(org, dest, removeFile)
-logging.info('Done.')
--- newgame 0.8/fonts.py
+++
@@ -1,6 +0,0 @@
-from pygame import font
-from os import sep
-
-font.init()
-
-normal=font.Font("fonts"+sep+"aquaduct"+sep+"Aqua.ttf", 22)
