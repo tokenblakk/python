14c1ec9b42c6177e10b5d2e51a5fa623198308bb
Tramel Jones <tokenblakk@gmail.com>
2015-04-30 20:43:48 -0500

    removed gify279. Giffer is the proper version

--- Gify279/Gif.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'tjones'
-
-from moviepy.editor import *
-
-clip = (VideoFileClip("frozen_trailer.mp4")
-        .subclip((1,22.65),(1,23.2))
-        .resize(0.3))
-clip.write_gif("use_your_head.gif")
